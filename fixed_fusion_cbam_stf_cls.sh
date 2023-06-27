ccc7557de84dc5e38169c45e6faa939def09591b
Lu Gan <ganlu@umich.edu>
2023-06-26 17:21:13 -0700

    minor change

--- fixed_fusion_cbam_stf_cls.sh
+++ fixed_fusion_cbam_stf_cls.sh
@@ -28,9 +28,9 @@
 # export CUDA_VISIBLE_DEVICES=1
 
 ######################################### OLD #################################################
-# python validate_all_att_cls.py /media/hdd2/rgb_gated_aligned --dataset stf_full --num-scenes 7 \
+# python validate_all_att_cls.py /media/hdd2/rgb_gated_aligned --dataset stf_snow_rain --num-scenes 7 \
 # --checkpoint /home/ganlu/trained_models_dsf/stf/output/train_stf/EXP_STF_FULL_CBAM/model_best.pth.tar \
-# --checkpoint-cls /home/ganlu/workspace/efficientdet-pytorch/output/train/stf-rgb-backbone-cls/model_best.pth.tar \
+# --checkpoint-cls /home/ganlu/workspace/efficientdet-pytorch/output/train/stf-rgb-backbone-cls-old/model_best.pth.tar \
 # --checkpoint-scenes /home/ganlu/trained_models_dsf/stf/output/train_stf/EXP_STF_FULL_CBAM/model_best.pth.tar \
 # /home/ganlu/trained_models_dsf/stf/output/train_stf/EXP_STF_CLEAR_DAY_CBAM/model_best.pth.tar \
 # /home/ganlu/trained_models_dsf/stf/output/train_stf/EXP_STF_CLEAR_NIGHT_CBAM/model_best.pth.tar \
@@ -44,7 +44,7 @@
 # echo "" | tee -a fixed_fusion_cbam_stf_cls.txt
 
 
-# python validate_all_att.py /media/hdd2/rgb_gated_aligned --dataset stf_full  \
+# python validate_all_att.py /media/hdd2/rgb_gated_aligned --dataset stf_snow_rain \
 # --checkpoint /home/ganlu/trained_models_dsf/stf/output/train_stf/EXP_STF_FULL_CBAM/model_best.pth.tar \
 # --split test --num-classes 4 \
 # --model efficientdetv2_dt --batch-size=8 --branch fusion --att_type cbam | tee fixed_fusion_cbam_stf_pascal.txt
@@ -52,7 +52,7 @@
 # echo "" | tee -a fixed_fusion_cbam_stf_pascal.txt
 
 
-######################################### NEW #################################################
+####################################### NEW #################################################
 python validate_all_att_cls.py /media/hdd2/rgb_gated_aligned --dataset stf_clear --num-scenes 3 \
 --checkpoint /home/ganlu/trained_stf_clear/EXP_CLEAR3_STF_CLEAR_CBAM/model_best.pth.tar \
 --checkpoint-cls /home/ganlu/workspace/efficientdet-pytorch/output/train/stf-rgb-backbone-cls/model_best.pth.tar \
@@ -65,9 +65,9 @@ python validate_all_att_cls.py /media/hdd2/rgb_gated_aligned --dataset stf_clear
 echo "" | tee -a fixed_fusion_cbam_stf_cls.txt
 
 
-python validate_all_att.py /media/hdd2/rgb_gated_aligned --dataset stf_clear  \
---checkpoint /home/ganlu/trained_stf_clear/EXP_CLEAR3_STF_CLEAR_CBAM/model_best.pth.tar \
---split test --num-classes 4 \
---model efficientdetv2_dt --batch-size=8 --branch fusion --att_type cbam | tee fixed_fusion_cbam_stf_pascal.txt
+# python validate_all_att.py /media/hdd2/rgb_gated_aligned --dataset stf_clear \
+# --checkpoint /home/ganlu/trained_stf_clear/EXP_CLEAR3_STF_CLEAR_CBAM/model_best.pth.tar \
+# --split test --num-classes 4 \
+# --model efficientdetv2_dt --batch-size=8 --branch fusion --att_type cbam | tee fixed_fusion_cbam_stf_pascal.txt
 
-echo "" | tee -a fixed_fusion_cbam_stf_pascal.txt
+# echo "" | tee -a fixed_fusion_cbam_stf_pascal.txt
